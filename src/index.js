e055da3bcd59812917cc241077bd4b511f31c0e0
MeStessa <72699640+MeStessa@users.noreply.github.com>
2020-11-24 14:32:12 +0100

    Added relevant icons from API

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ if (minutes < 10) {
   minutes = `0${minutes}`;
 }
 let currentTime = document.querySelector("#current-time");
-currentTime.innerHTML = `Now ${hour}:${minutes}`;
+currentTime.innerHTML = `Last updated on  ${hour}:${minutes}`;
 
 let days = [
   "Sunday",
@@ -40,8 +40,10 @@ function checkFahr(event) {
 let fahr = document.querySelector("#fahr");
 fahr.addEventListener("click", checkFahr);
 
+
+
 function showDetails(response) {
-  console.log(response);
+  
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#current-temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -56,15 +58,17 @@ function showDetails(response) {
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
 
-  console.log(response);
+  console.log(response.data);
   document.querySelector("#percieved").innerHTML = Math.round(
     response.data.main.feels_like
   );
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
-  );
+  );  
+  document.querySelector("#main-icon").setAttribute("src" , `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
+
 
 function searchCity(city) {
   let apiKey = "2abf5cd5bdf12c255e9d60ca40791365";
@@ -93,7 +97,7 @@ function getPosition(position) {
   let apiKey = "2abf5cd5bdf12c255e9d60ca40791365";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showDetails);
-  console.log(lat, lon);
+  
 }
 let currentButton = document.querySelector("#current-button");
 currentButton.addEventListener("click", showPosition);
