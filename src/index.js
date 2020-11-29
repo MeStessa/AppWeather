d6709ee9d8ca2571bd5b3fbac973d1dd64aed1f9
MeStessa <72699640+MeStessa@users.noreply.github.com>
2020-11-29 15:27:58 +0100

    Implemented hour forecast

--- src/index.js
+++ src/index.js
@@ -23,6 +23,20 @@ let day = days[now.getDay()];
 let today = document.querySelector("#today");
 today.innerHTML = `${day}`;
 
+function formatHours(timestamp){
+
+  let now = new Date(timestamp);
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+  return `${hour}:${minutes}`
+}
+
 function checkCelsius(event) {
   event.preventDefault();
   let temperatureCelsius = document.querySelector("#current-temperature");
@@ -50,7 +64,7 @@ fahr.addEventListener("click", checkFahr);
 
 function showDetails(response) {
   
-  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#city").innerHTML = response.data.name + `, ` + response.data.sys.country;
   document.querySelector("#current-temperature").innerHTML = Math.round(
     response.data.main.temp
   );
@@ -64,7 +78,7 @@ function showDetails(response) {
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
 
-  console.log(response.data);
+
   document.querySelector("#percieved").innerHTML = Math.round(
     response.data.main.feels_like
   );
@@ -80,11 +94,29 @@ function showDetails(response) {
 }
 
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML +=`<div class="col-2">
+            <p class="card-text">${formatHours(forecast.dt * 1000)}</p>
+            <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" width="50px" />
+            <p class="details"> ${Math.round(forecast.main.temp_max)}° </br>  ${Math.round(forecast.main.temp_min)}°</p>
+          </div>`;
+
+}
+}
 function searchCity(city) {
   let apiKey = "2abf5cd5bdf12c255e9d60ca40791365";
 
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showDetails);
+
+  let apiLink= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiLink).then(displayForecast);
 }
 
 function cityDefine(event) {
@@ -113,5 +145,4 @@ function getPosition(position) {
 }
 let currentButton = document.querySelector("#current-button");
 currentButton.addEventListener("click", showPosition);
-
 
